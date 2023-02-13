bda455f334fc001693a61cc4e517b3d576a34bf6
Slumhunden <kroghbastin@gmail.com>
2023-02-13 14:13:56 +0100

    billede slette

---
+++ app.js
@@ -0,0 +1,11 @@
+//document.body.innerHTML+= "Fedt";
+
+//function showtext() {
+  /* document.querySelector("antal2").textContent =
+    "Opskriften er beregnet til 2, gang det selv op :-)";
+}
+document.querySelector("antal2").addEventListener("click", sayHello);
+ */
+
+function myFunction() {
+  document.getElementById("demo").innerHTML = "Hello World";}
